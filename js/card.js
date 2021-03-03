111b5c58e3427dcf68314ea91338dc7d8600b74c
Евгения <april-may@mail.ru>
2021-03-03 23:10:02 +0300

    Задание 5.1 - DOM-элементы: features, photos, параметры фукнции createCard

--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-import { similarArray } from './main.js';
+// import { similarArray } from './main.js';
 
 //   Сопоставление типов жилья с подписями
 const offerTypesMap = {
@@ -25,62 +25,80 @@ const hideElement = function (className) {
 }
 
 
+// функция features
+const createFeatureList = function (features) {
+  const fragmentElement = document.createDocumentFragment();
+
+  for (let i = 0; i < features.length; i++) {
+    const featureItem = document.createElement('li');
+    featureItem.classList.add('popup__feature--' + features[i], 'popup__feature');
+    fragmentElement.appendChild(featureItem);
+  }
+  return fragmentElement;
+}
+
+
+// функция photos
+const createPhotosList = function (photos) {
+  const fragmentElement = document.createDocumentFragment();
+
+  for (let j = 0; j < photos.length; j++) {
+    const imageItem = document.createElement('img');
+    imageItem.classList.add('popup__photo');
+    imageItem.setAttribute('src', photos[j]);
+    imageItem.setAttribute('width', '45');
+    imageItem.setAttribute('height', '40');
+    imageItem.setAttribute('alt', 'Фотография жилья');
+    fragmentElement.appendChild(imageItem);
+  }
+  return fragmentElement;
+}
+
 
 //   Функция, которая принимает в себя одно объявление и заполняет одну карточку
-const createCard = function (add = similarArray[0]) {
+
+const createCard = function (offer, author) {
   const newCardElement = cardElement.cloneNode(true);
 
   // title
   // проверка заполняемости данных
   const titleElement = newCardElement.querySelector('.popup__title');
   if (titleElement) {
-    titleElement.textContent = add.offer.title;
+    titleElement.textContent = offer.title;
   } else {
     hideElement('.popup__title');
   }
 
 
-  newCardElement.querySelector('.popup__text--address').textContent = add.offer.address;
-  newCardElement.querySelector('.popup__text--price').textContent = add.offer.price + ' ₽/ночь';
-  newCardElement.querySelector('.popup__type').textContent = offerTypesMap[add.offer.type];
+  newCardElement.querySelector('.popup__text--address').textContent = offer.address;
+  newCardElement.querySelector('.popup__text--price').textContent = offer.price + ' ₽/ночь';
+  newCardElement.querySelector('.popup__type').textContent = offerTypesMap[offer.type];
 
-  const rooms = add.offer.rooms;
-  const guests = add.offer.guests;
+  const rooms = offer.rooms;
+  const guests = offer.guests;
   newCardElement.querySelector('.popup__text--capacity').textContent = rooms + (rooms === 1 ? ' комнатa для ' : ((rooms === 2 || rooms === 3 || rooms === 4) ? ' комнаты для ' : ' комнат для ')) + guests + (guests === 1 ? ' гостя' : ' гостей');
 
-  newCardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + add.offer.checkin + ' выезд до ' + add.offer.checkout;
+  newCardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ' выезд до ' + offer.checkout;
 
 
   //  features
   const featureListElement = newCardElement.querySelector('.popup__features');
-  featureListElement.innerHTML = '';   // Очищение элемента
-  for (let i = 0; i < add.offer.features.length; i++) {
-    const featureItem = document.createElement('li');
-    featureItem.classList.add('popup__feature--' + add.offer.features[i], 'popup__feature');
-    featureListElement.appendChild(featureItem);
-  }
+  featureListElement.innerHTML = '';
+  featureListElement.appendChild(createFeatureList(offer.features));
 
 
   //  photos
   const photoListElement = newCardElement.querySelector('.popup__photos');
   photoListElement.innerHTML = '';
-  for (let j = 0; j < add.offer.photos.length; j++) {
-    const imageItem = document.createElement('img');
-    imageItem.classList.add('popup__photo');
-    imageItem.setAttribute('src', add.offer.photos[j]);
-    imageItem.setAttribute('width', '45');
-    imageItem.setAttribute('height', '40');
-    imageItem.setAttribute('alt', 'Фотография жилья');
-    photoListElement.appendChild(imageItem);
-  }
+  featureListElement.appendChild(createPhotosList(offer.photos));
 
 
   //  description
-  newCardElement.querySelector('.popup__description').textContent = add.offer.description;
+  newCardElement.querySelector('.popup__description').textContent = offer.description;
 
 
   //  avatar
-  newCardElement.querySelector('.popup__avatar').src = add.author.avatar;
+  newCardElement.querySelector('.popup__avatar').src = author.avatar;
 
 
   //  Возврат карточки
--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 import './util.js';
 import './data.js';
 import './card.js';
+import './form.js';
+
 
 
 import { createArrayАdvertising } from './data.js';
 import { createCard, mapCanvas } from './card.js';
 
 const similarArray = createArrayАdvertising();
+
 const card = createCard(similarArray[0]);
 
 
@@ -14,4 +17,4 @@ const card = createCard(similarArray[0]);
 mapCanvas.append(card);
 
 
-export { similarArray };
+// export { similarArray };
